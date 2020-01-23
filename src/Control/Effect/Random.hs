a1d9654c36f4293cf6a7892df037d62dbd634b3c
Rob Rix <rob.rix@me.com>
2020-01-23 18:30:21 -0500

    Tidy up the HFunctor & Effect instances.

--- src/Control/Effect/Random.hs
+++ src/Control/Effect/Random.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE StandaloneDeriving #-}
 module Control.Effect.Random
 ( -- * Random effect
@@ -25,15 +26,17 @@ data Random m k
 deriving instance Functor m => Functor (Random m)
 
 instance HFunctor Random where
-  hmap f (Random       k) = Random           (f . k)
-  hmap f (RandomR r    k) = RandomR r        (f . k)
-  hmap f (Interleave m k) = Interleave (f m) (f . k)
+  hmap f = \case
+    Random       k -> Random           (f . k)
+    RandomR r    k -> RandomR r        (f . k)
+    Interleave m k -> Interleave (f m) (f . k)
   {-# INLINE hmap #-}
 
 instance Effect Random where
-  thread state handler (Random       k) = Random                            (handler . (<$ state) . k)
-  thread state handler (RandomR r    k) = RandomR r                         (handler . (<$ state) . k)
-  thread state handler (Interleave m k) = Interleave (handler (m <$ state)) (handler . fmap k)
+  thread state handler = \case
+    Random       k -> Random                            (handler . (<$ state) . k)
+    RandomR r    k -> RandomR r                         (handler . (<$ state) . k)
+    Interleave m k -> Interleave (handler (m <$ state)) (handler . fmap k)
   {-# INLINE thread #-}
 
 
