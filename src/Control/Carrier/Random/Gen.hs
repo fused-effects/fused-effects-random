2abca77754c7e63070a245889cf23724cb0e3ad0
Rob Rix <rob.rix@me.com>
2020-03-13 18:10:57 -0400

    Use StateT instead of StateC to require less swapping.

--- src/Control/Carrier/Random/Gen.hs
+++ src/Control/Carrier/Random/Gen.hs
@@ -17,13 +17,14 @@ module Control.Carrier.Random.Gen
 
 import           Control.Algebra
 import           Control.Applicative (Alternative)
-import           Control.Carrier.State.Strict
 import           Control.Effect.Random
+import           Control.Effect.State
 import           Control.Monad (MonadPlus)
 import qualified Control.Monad.Fail as Fail
 import           Control.Monad.Fix
 import           Control.Monad.IO.Class (MonadIO(..))
 import           Control.Monad.Trans.Class
+import qualified Control.Monad.Trans.State.Strict as S
 import           Data.Tuple (swap)
 import qualified System.Random as R (Random(..), RandomGen(..), StdGen, newStdGen)
 
@@ -32,8 +33,8 @@ import qualified System.Random as R (Random(..), RandomGen(..), StdGen, newStdGe
 -- @
 -- 'runRandom' g ('pure' b) = 'pure' (g, b)
 -- @
-runRandom :: g -> RandomC g m a -> m (g, a)
-runRandom g = runState g . runRandomC
+runRandom :: Functor m => g -> RandomC g m a -> m (g, a)
+runRandom g = fmap swap . (`S.runStateT` g) . runRandomC
 {-# INLINE runRandom #-}
 
 -- | Run a random computation starting from a given generator and discarding the final generator.
@@ -59,7 +60,7 @@ evalRandomSystem :: MonadIO m => RandomC R.StdGen m a -> m a
 evalRandomSystem m = liftIO R.newStdGen >>= flip evalRandom m
 {-# INLINE evalRandomSystem #-}
 
-newtype RandomC g m a = RandomC { runRandomC :: StateC g m a }
+newtype RandomC g m a = RandomC { runRandomC :: S.StateT g m a }
   deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus, MonadTrans)
 
 instance (Algebra sig m, R.RandomGen g) => Algebra (Random :+: sig) (RandomC g m) where
@@ -73,5 +74,5 @@ instance (Algebra sig m, R.RandomGen g) => Algebra (Random :+: sig) (RandomC g m
     R other          -> RandomC (alg (runRandomC . hdl) (R other) ctx)
     where
     state :: (g -> (a, g)) -> RandomC g m a
-    state f = RandomC (StateC (pure . swap . f))
+    state f = RandomC (S.StateT (pure . f))
   {-# INLINE alg #-}
