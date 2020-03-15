a503f221bacf342f70e9a22e8977ca909650577b
Rob Rix <rob.rix@me.com>
2020-03-15 18:07:09 -0400

    Use the Church-encoded State carrier.

--- src/Control/Carrier/Random/Gen.hs
+++ src/Control/Carrier/Random/Gen.hs
@@ -17,15 +17,13 @@ module Control.Carrier.Random.Gen
 
 import           Control.Algebra
 import           Control.Applicative (Alternative)
+import           Control.Carrier.State.Church
 import           Control.Effect.Random
-import           Control.Effect.State
 import           Control.Monad (MonadPlus)
 import qualified Control.Monad.Fail as Fail
 import           Control.Monad.Fix
 import           Control.Monad.IO.Class (MonadIO(..))
 import           Control.Monad.Trans.Class
-import qualified Control.Monad.Trans.State.Strict as S
-import           Data.Tuple (swap)
 import qualified System.Random as R (Random(..), RandomGen(..), StdGen, newStdGen)
 
 -- | Run a random computation starting from a given generator.
@@ -33,8 +31,8 @@ import qualified System.Random as R (Random(..), RandomGen(..), StdGen, newStdGe
 -- @
 -- 'runRandom' g ('pure' b) = 'pure' (g, b)
 -- @
-runRandom :: Functor m => g -> RandomC g m a -> m (g, a)
-runRandom g = fmap swap . (`S.runStateT` g) . runRandomC
+runRandom :: Applicative m => g -> RandomC g m a -> m (g, a)
+runRandom g = runState (curry pure) g . runRandomC
 {-# INLINE runRandom #-}
 
 -- | Run a random computation starting from a given generator and discarding the final generator.
@@ -42,8 +40,8 @@ runRandom g = fmap swap . (`S.runStateT` g) . runRandomC
 -- @
 -- 'evalRandom' g ('pure' b) = 'pure' b
 -- @
-evalRandom :: Functor m => g -> RandomC g m a -> m a
-evalRandom g = fmap snd . runRandom g
+evalRandom :: Applicative m => g -> RandomC g m a -> m a
+evalRandom g = evalState g . runRandomC
 {-# INLINE evalRandom #-}
 
 -- | Run a random computation starting from a given generator and discarding the final result.
@@ -51,8 +49,8 @@ evalRandom g = fmap snd . runRandom g
 -- @
 -- 'execRandom' g ('pure' b) = g
 -- @
-execRandom :: Functor m => g -> RandomC g m a -> m g
-execRandom g = fmap fst . runRandom g
+execRandom :: Applicative m => g -> RandomC g m a -> m g
+execRandom g = execState g . runRandomC
 {-# INLINE execRandom #-}
 
 -- | Run a random computation in 'IO', splitting the global standard generator to get a new one for the computation.
@@ -60,7 +58,7 @@ evalRandomSystem :: MonadIO m => RandomC R.StdGen m a -> m a
 evalRandomSystem m = liftIO R.newStdGen >>= flip evalRandom m
 {-# INLINE evalRandomSystem #-}
 
-newtype RandomC g m a = RandomC { runRandomC :: S.StateT g m a }
+newtype RandomC g m a = RandomC { runRandomC :: StateC g m a }
   deriving (Alternative, Applicative, Functor, Monad, Fail.MonadFail, MonadFix, MonadIO, MonadPlus, MonadTrans)
 
 instance (Algebra sig m, R.RandomGen g) => Algebra (Random :+: sig) (RandomC g m) where
@@ -74,5 +72,5 @@ instance (Algebra sig m, R.RandomGen g) => Algebra (Random :+: sig) (RandomC g m
     R other          -> RandomC (alg (runRandomC . hdl) (R other) ctx)
     where
     state :: (g -> (a, g)) -> RandomC g m a
-    state f = RandomC (S.StateT (pure . f))
+    state f = RandomC (StateC (\ k -> uncurry (flip k) . f))
   {-# INLINE alg #-}
